6ad34c48d0bb50be5b9f9b47e9986008495163c7
David Rios <drios9107@gmail.com>
2025-01-05 09:42:58 -0500

    created DashboardSkeleton component

---
+++ src/components/DashboardSkeleton.js
@@ -0,0 +1,16 @@
+import { Box, Skeleton, useMediaQuery } from '@mui/material'
+import React from 'react'
+
+const DashboardSkeleton = () => {
+    const isMobile = useMediaQuery("@media (max-width:500px)");
+
+    return <Box sx={{ width: '100%', display: 'flex', flexDirection: 'column', gap: isMobile ? '10px' : '25px' }}>
+        <Skeleton variant='rounded' width={'100%'} height={'130px'} animation='wave' />
+        <Box sx={{ display: 'flex', flexDirection: 'row', gap: isMobile ? '10px' : '25px' }}>
+            <Skeleton variant='rounded' width={'100%'} height={'300px'} animation='wave' />
+            <Skeleton variant='rounded' width={'100%'} height={'300px'} animation='wave' />
+        </Box>
+    </Box>
+}
+
+export default DashboardSkeleton
