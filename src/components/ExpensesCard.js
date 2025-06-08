f6c0b006f6e67a73cde2a205bf19dfe267d241c6
David Rios <drios9107@gmail.com>
2025-06-08 15:55:48 -0400

    integrated IncomeExpensesContainer component

--- src/components/ExpensesCard.js
+++ src/components/ExpensesCard.js
@@ -1,35 +1,35 @@
 import { useDashboardContext } from '@/hooks';
 import { useFormat } from '@/hooks/useFormat';
 import { useTranslation } from '@/hooks/useTranslation';
-import { Box, Card, Typography } from '@mui/material'
+import { Typography } from '@mui/material';
 import { useParams } from 'next/navigation';
-import { useState } from 'react';
+import { useMemo } from 'react';
+import IncomeExpensesContainer from './IncomeExpensesContainer';
 
 const ExpensesCard = () => {
     const { monthExpenses = 0, monthIncome = 0 } = useDashboardContext();
-    const [isHover, setIsHover] = useState(false)
     const { currencyFormat } = useFormat();
     const params = useParams();
     const { t } = useTranslation(params?.lng ?? 'en', 'dashboard')
 
-    return <Card sx={styles.container} elevation={isHover ? 3 : 1} onMouseEnter={() => setIsHover(true)} onMouseLeave={() => setIsHover(false)}>
-        <Box sx={styles.row}>
+    const leftSide = useMemo(() => {
+        return <>
             <Typography variant='body2' sx={{ userSelect: 'none' }}>{t("expenses")}</Typography>
             <Typography variant='body2' sx={{ userSelect: 'none' }}>{t("income")}</Typography>
-        </Box>
-        <Box sx={styles.row}>
+        </>
+    }, [t])
+
+    const rightSide = useMemo(() => {
+        return <>
             <Typography variant='body2' sx={{ fontWeight: 600, textAlign: 'right', textShadow: '1px 2px 3px salmon' }} >{currencyFormat(monthExpenses)} $</Typography>
             <Typography variant='body2' sx={{ fontWeight: 600, textAlign: 'right', textShadow: '1px 2px 3px limegreen' }}>{currencyFormat(monthIncome)} $</Typography>
-        </Box>
-    </Card>
+        </>
+    }, [currencyFormat, monthExpenses, monthIncome])
+
+    return <IncomeExpensesContainer
+        leftSide={leftSide}
+        rightSide={rightSide}
+    />
 }
 
 export default ExpensesCard
-
-const styles = {
-    container: {
-        width: '250px', height: '80px', px: '16px', display: 'flex', flexDirection: 'row', justifyContent: 'space-between',
-        alignItems: 'center', gap: '15px', backgroundColor: '#D6E6FF', '&:hover': { opacity: 0.7 }
-    },
-    row: { display: 'flex', flexDirection: 'column', gap: '5px' },
-}
--- src/components/IncomeCard.js
+++ src/components/IncomeCard.js
@@ -1,36 +1,37 @@
 import { useDashboardContext } from '@/hooks';
 import { useFormat } from '@/hooks/useFormat';
 import { useTranslation } from '@/hooks/useTranslation';
-import { Box, Card, Typography } from '@mui/material'
+import { Typography } from '@mui/material';
 import moment from 'moment'
 import { useParams } from 'next/navigation';
-import { useState } from 'react';
+import { useMemo } from 'react';
+import IncomeExpensesContainer from './IncomeExpensesContainer';
+
 
 const IncomeCard = () => {
     const { biggestIncome = 0, biggestIncomeDate } = useDashboardContext();
-    const [isHover, setIsHover] = useState(false)
     const params = useParams();
     const { t } = useTranslation(params?.lng ?? 'en', 'dashboard')
     const { currencyFormat } = useFormat();
 
-    return <Card sx={styles.container} elevation={isHover ? 3 : 1} onMouseEnter={() => setIsHover(true)} onMouseLeave={() => setIsHover(false)}>
-        <Box sx={styles.row}>
+    const leftSide = useMemo(() => {
+        return <>
             <Typography variant='body2' sx={{ userSelect: 'none' }}>{t('biggestIncome')}</Typography>
             <Typography variant='body2' sx={{ userSelect: 'none' }}>{t('date')}</Typography>
-        </Box>
-        <Box sx={styles.row}>
+        </>
+    }, [t])
+
+    const rightSide = useMemo(() => {
+        return <>
             <Typography variant='body2' sx={{ fontWeight: 600, textAlign: 'right' }}>{currencyFormat(biggestIncome)} $</Typography>
             <Typography variant='body2' sx={{ fontWeight: 600, textAlign: 'right' }}>{biggestIncomeDate ? moment(biggestIncomeDate).format('DD/MM/YYYY') : '-'}</Typography>
-        </Box>
-    </Card>
+        </>
+    }, [biggestIncome, biggestIncomeDate, currencyFormat])
+
+    return <IncomeExpensesContainer
+        leftSide={leftSide}
+        rightSide={rightSide}
+    />
 }
 
 export default IncomeCard
-
-const styles = {
-    container: {
-        width: '250px', height: '80px', px: '16px', display: 'flex', flexDirection: 'row', justifyContent: 'space-between',
-        alignItems: 'center', gap: '15px', backgroundColor: '#D6E6FF', '&:hover': { opacity: 0.7 }
-    },
-    row: { display: 'flex', flexDirection: 'column', gap: '5px' },
-}
