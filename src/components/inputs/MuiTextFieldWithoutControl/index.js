c72ea8c1e089741f4574c17db8502210c2213f8b
David Rios <drios9107@gmail.com>
2025-07-23 01:37:15 -0400

    added props

--- src/components/inputs/MuiTextFieldWithoutControl/index.js
+++ src/components/inputs/MuiTextFieldWithoutControl/index.js
@@ -1,5 +1,5 @@
 import ShowFieldErrors from '@/components/ShowFieldErrors'
-import { FormControl, TextField } from '@mui/material';
+import { FormControl, InputAdornment, TextField } from '@mui/material';
 
 const MuiTextfieldWithoutControl = ({ fieldName, state, setState = () => { }, errors, options = {} }) => {
     return <FormControl>
@@ -7,9 +7,15 @@ const MuiTextfieldWithoutControl = ({ fieldName, state, setState = () => { }, er
             defaultValue={state}
             onChange={v => setState(v?.target?.value)}
             size='small'
-            {...options}
             rows={options?.multiline ? options?.rows ?? 3 : 1}
             maxRows={Infinity}
+            slotProps={{
+                input: {
+                    endAdornment: options?.endAdornment ? <InputAdornment position="end">{options?.endAdornment}</InputAdornment> : null,
+                    startAdornment: options?.startAdornment ? <InputAdornment position="start">{options?.startAdornment}</InputAdornment> : null
+                },
+            }}
+            {...options}
         />
         <ShowFieldErrors errors={errors} fieldName={fieldName} />
     </FormControl>
