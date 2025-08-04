b8df991846abfebe0c8b4144da49984ce60353a5
David Rios <drios9107@gmail.com>
2025-08-04 11:56:32 -0400

    added language selector rendered icon

--- src/components/LanguageSelector.js
+++ src/components/LanguageSelector.js
@@ -1,45 +1,61 @@
-import { useCallback, useEffect, useMemo, useState } from 'react';
+import { useCallback, useMemo } from 'react';
 import MuiSingleSelectFieldWithoutControl from './inputs/MuiSingleSelectFieldWithoutControl';
 import { useParams, usePathname, useRouter } from 'next/navigation';
 import { useTranslation } from '@/hooks/useTranslation';
+import Flag from "react-world-flags";
 
 const LanguageSelector = ({ options = {} }) => {
     const { lng = 'en' } = useParams()
     const { t } = useTranslation(lng, 'common')
     const pathname = usePathname()
     const router = useRouter()
-    const [language, setLanguage] = useState()
-    console.log('***', { language, lng, pathname })
-    useEffect(() => {
-        if (lng)
-            setLanguage(lng)
-    }, [lng])
 
     const languageList = useMemo(() => {
         return [
-            { _id: 'es', name: t('es') },
-            { _id: 'en', name: t('en') }
+            { _id: 'es', name: t('es'), code: 'es' },
+            { _id: 'en', name: t('en'), code: 'us' }
         ]
     }, [t])
 
+    const getLanguage = useCallback(id => {
+        return languageList.find(i => i?._id === id)
+    }, [languageList])
+
     const onChange = useCallback((v) => {
         const value = v?.target?.value;
-        setLanguage(value);
         const splittedPath = pathname?.split('/')
 
-        if (languageList.findIndex(i => i?._id === splittedPath?.[1]) > -1) {
+        if (getLanguage(splittedPath?.[1])) {
             const path = splittedPath?.[2]
             router.push(`/${value}/${path}`)
         }
-    }, [languageList, pathname, router])
+    }, [getLanguage, pathname, router])
 
     return <MuiSingleSelectFieldWithoutControl
         fieldName={'language'}
-        state={language}
+        state={lng}
         setState={onChange}
         list={languageList}
-        options={{ sx: { width: '70px' }, ...options }}
+        options={{
+            sx: styles.selector,
+            renderValue: v => <Flag code={getLanguage(v)?.code} height={20} width={25} />,
+            ...options
+        }}
     />
 }
 
 export default LanguageSelector
+
+const styles = {
+    selector: {
+        width: '60px',
+        cursor: 'pointer',
+        '& :hover': { opacity: 0.7 },
+        '& fieldset': { border: 'none' },
+        '& .MuiSelect-select': {
+            padding: '0px',
+            alignItems: 'center',
+            display: 'flex'
+        }
+    }
+}
