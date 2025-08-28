cf9a0e9b5a00314c76b309490baebefa03a3e69e
David Rios <drios9107@gmail.com>
2025-08-28 13:46:13 -0400

    redefined details view

--- src/components/crud/defaultTransactionValue/Details.js
+++ src/components/crud/defaultTransactionValue/Details.js
@@ -1,18 +1,18 @@
+import DetailsDataRow from '@/components/DetailsDataRow'
 import DetailsDrawer from '@/components/DetailsDrawer'
-import { Box, Typography } from '@mui/material'
-import React from 'react'
+import { useTranslation } from '@/hooks/useTranslation'
+import { useParams } from 'next/navigation'
 
 const Details = ({ item, onClose = () => { } }) => {
-    return <DetailsDrawer onClose={onClose}>
-        <Box sx={styles.row}>
-            <Typography variant="body1" sx={{ fontWeight: '600' }}>Name: </Typography>
-            <Typography variant="body1">{item.name}</Typography>
-        </Box>
+    const params = useParams()
+    const { t } = useTranslation(params?.lng ?? 'en', 'transactions')
+
+    return <DetailsDrawer onClose={onClose} extraclasses={{ display: 'flex', gap: '10px', flexDirection: 'column' }}>
+        <DetailsDataRow value={item.category?.name} title={t('category')} />
+        <DetailsDataRow value={item.subCategory?.name} title={t('subCategory')} />
+        {item?.amount > 0 && <DetailsDataRow value={item.amount} title={t('amount')} />}
+        {(item?.description && item?.description !== '') && <DetailsDataRow value={item.description} title={t('description')} />}
     </DetailsDrawer>
 }
 
 export default Details
-
-const styles = {
-    row: { display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'flex-start', gap: '5px' },
-}
