8b33c7a159bfb9a48a19ca96d2e4e3204b145376
David Rios <drios9107@gmail.com>
2025-09-20 15:19:40 -0400

    added icon to indevelopment badge

--- src/components/InDevelopmentBadge.js
+++ src/components/InDevelopmentBadge.js
@@ -1,7 +1,8 @@
+import { Error } from '@mui/icons-material';
 import { Chip } from '@mui/material';
 
 const InDevelopmentBadge = () => {
-    return <Chip sx={styles.chip} label='In Development' color='error' variant='outlined' />
+    return <Chip sx={styles.chip} label='In Development' color='error' variant='outlined' icon={<Error fontSize='small' />} />
 }
 
 export default InDevelopmentBadge
