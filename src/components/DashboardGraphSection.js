97d08b8b20b0f38b25724c2ab6e77ad0271b5833
David Rios <drios9107@gmail.com>
2026-02-16 22:18:22 -0500

    removed unused item in DashboardGraphSection

--- src/components/DashboardGraphSection.js
+++ src/components/DashboardGraphSection.js
@@ -7,12 +7,12 @@ const DashboardGraphSection = ({ conditionalGraphStyles, isHover1, setIsHover1,
     currentMonth, currentYear, isHover2, setIsHover2 }) => {
 
     return <Grid2 container spacing={2} sx={fadeInStyles()}>
-        <Grid2 item size={{ xs: 12, md: 6 }}>
+        <Grid2 size={{ xs: 12, md: 6 }}>
             <Paper sx={[styles.graph, conditionalGraphStyles]} elevation={isHover1 ? 3 : 1} onMouseEnter={() => setIsHover1(true)} onMouseLeave={() => setIsHover1(false)}>
                 <DashboardBarGraph currentMonth={currentMonth} currentYear={currentYear} />
             </Paper>
         </Grid2>
-        <Grid2 item size={{ xs: 12, md: 6 }}>
+        <Grid2 size={{ xs: 12, md: 6 }}>
             <Paper sx={[styles.graph, conditionalGraphStyles]} elevation={isHover2 ? 3 : 1} onMouseEnter={() => setIsHover2(true)} onMouseLeave={() => setIsHover2(false)}>
                 <DashboardPieGraph currentMonth={currentMonth} currentYear={currentYear} />
             </Paper>
