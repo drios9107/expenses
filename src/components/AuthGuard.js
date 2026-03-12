9f8a2d60c7ba9c801b9446f872023589149c0990
David Rios <drios9107@gmail.com>
2026-03-12 13:25:57 -0400

    added login error

--- src/components/AuthGuard.js
+++ src/components/AuthGuard.js
@@ -1,55 +1,92 @@
-'use client';
-import { useSession } from 'next-auth/react';
-import { useParams, usePathname, useRouter, useSearchParams } from 'next/navigation';
-import { useEffect, useMemo } from 'react';
-import FallbackLoader from './FallbackLoader';
-import { combinedPublicRoutes, completeAdminRoutes, policyRoutes, policyRoutesWithLanguage, publicRoutes } from '@/utils/helpers';
-import { setAuthToken } from '@/utils/AxiosInterceptor';
+'use client'
+import { useSession } from 'next-auth/react'
+import { useParams, usePathname, useRouter, useSearchParams } from 'next/navigation'
+import { useEffect, useMemo, useRef } from 'react'
+import FallbackLoader from './FallbackLoader'
+import {
+	combinedPublicRoutes,
+	completeAdminRoutes,
+	policyRoutes,
+	policyRoutesWithLanguage,
+	publicRoutes
+} from '@/utils/helpers'
+import { setAuthToken } from '@/utils/AxiosInterceptor'
+import { useToast } from '@/hooks/useToast'
+import { useTranslation } from '@/hooks/useTranslation'
 
 export default function AuthGuard({ children, params }) {
-    const { data: session, status } = useSession()
+	const { data: session, status } = useSession()
+	const { toastError } = useToast()
+	const { t } = useTranslation(params?.lng, 'login')
 
-    const router = useRouter();
-    const pathname = usePathname();
-    const { lng } = useParams()
-    const searchParams = useSearchParams();
+	const searchParams = useSearchParams()
+	const error = searchParams.get('error')
+	const lastErrorRef = useRef()
 
-    const callbackUrl = useMemo(() => {
-        const url = searchParams.get('callbackUrl');
-        return url ? decodeURIComponent(url) : `/${lng}`;
-    }, [lng, searchParams]);
+	const router = useRouter()
+	const pathname = usePathname()
+	const { lng } = useParams()
 
-    const pathIsStrictLogin = useMemo(() => pathname === `/${lng}/login`, [lng, pathname])
-    const pathIsLandingPage = useMemo(() => pathname === `/${lng}`, [lng, pathname])
-    const pathIsContactPage = useMemo(() => pathname === `/${lng}/contact`, [lng, pathname])
-    const pathIsPoliciesPage = useMemo(() => policyRoutesWithLanguage.includes(pathname), [pathname])
+	const callbackUrl = useMemo(() => {
+		const url = searchParams.get('callbackUrl')
+		return url ? decodeURIComponent(url) : `/${lng}`
+	}, [lng, searchParams])
 
-    useEffect(() => {
-        if (!pathIsLandingPage && !pathIsContactPage && !pathIsPoliciesPage) {
-            if (status === 'unauthenticated' && publicRoutes)
-                router.push(`/${lng}/login?callbackUrl=${encodeURIComponent(pathname)}`);
-            else if (status === 'authenticated' && searchParams.has('callbackUrl'))
-                router.push(callbackUrl)
-            else if (status === 'authenticated' && pathIsStrictLogin)
-                router.push(`/${lng}`);
-        }
-    }, [callbackUrl, lng, pathIsContactPage, pathIsLandingPage, pathIsPoliciesPage, pathIsStrictLogin, pathname, router, searchParams, status]);
+	const pathIsStrictLogin = useMemo(() => pathname === `/${lng}/login`, [lng, pathname])
+	const pathIsLandingPage = useMemo(() => pathname === `/${lng}`, [lng, pathname])
+	const pathIsContactPage = useMemo(() => pathname === `/${lng}/contact`, [lng, pathname])
+	const pathIsPoliciesPage = useMemo(() => policyRoutesWithLanguage.includes(pathname), [pathname])
 
-    useEffect(() => {
-        if (session?.user?.token)
-            setAuthToken(session.user.token);
-    }, [session?.user?.token]);
+	useEffect(() => {
+		if (!error) {
+			lastErrorRef.current = null
+		} else if (error !== lastErrorRef.current) {
+			const loginError = t('login.login_error')
+			const tryVpn = t('login.try_vpn')
 
-    if (status === 'loading')
-        return <FallbackLoader />
+			if (loginError === 'login.login_error') return
 
-    if (status === 'authenticated' && session?.user?.role !== 'Admin' && completeAdminRoutes?.includes(pathname)) {
-        router.push(`/${lng}/dashboard`)
-        return <FallbackLoader />
-    }
+			if (error === 'OAuthCallback') {
+				toastError(`${loginError} ${error}. ${tryVpn}`)
+			} else {
+				toastError(`${loginError} ${error}`)
+			}
+			lastErrorRef.current = error
+		}
+	}, [error, t, toastError])
 
-    if (status === 'authenticated' || combinedPublicRoutes?.includes(pathname))
-        return <>{children}</>;
+	useEffect(() => {
+		if (!pathIsLandingPage && !pathIsContactPage && !pathIsPoliciesPage) {
+			if (status === 'unauthenticated' && publicRoutes)
+				router.push(`/${lng}/login?callbackUrl=${encodeURIComponent(pathname)}`)
+			else if (status === 'authenticated' && searchParams.has('callbackUrl')) router.push(callbackUrl)
+			else if (status === 'authenticated' && pathIsStrictLogin) router.push(`/${lng}`)
+		}
+	}, [
+		callbackUrl,
+		lng,
+		pathIsContactPage,
+		pathIsLandingPage,
+		pathIsPoliciesPage,
+		pathIsStrictLogin,
+		pathname,
+		router,
+		searchParams,
+		status
+	])
 
-    return null;
+	useEffect(() => {
+		if (session?.user?.token) setAuthToken(session.user.token)
+	}, [session?.user?.token])
+
+	if (status === 'loading') return <FallbackLoader />
+
+	if (status === 'authenticated' && session?.user?.role !== 'Admin' && completeAdminRoutes?.includes(pathname)) {
+		router.push(`/${lng}/dashboard`)
+		return <FallbackLoader />
+	}
+
+	if (status === 'authenticated' || combinedPublicRoutes?.includes(pathname)) return <>{children}</>
+
+	return null
 }
