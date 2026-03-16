5258f49bfe4d07147c5788c4b29d0e98d4a8339e
David Rios <drios9107@gmail.com>
2026-03-16 14:04:20 -0400

    handled all data in single description field

--- src/components/crud/transaction/RawMultiTransactions.js
+++ src/components/crud/transaction/RawMultiTransactions.js
@@ -1,11 +1,10 @@
 import SimpleModal from '@/components/SimpleModal'
-import { Box, Button } from '@mui/material'
-import { useCallback, useDeferredValue, useEffect, useState } from 'react'
+import { Box } from '@mui/material'
+import { useCallback, useState } from 'react'
 import { useTransaction } from '@/hooks'
 import { useParams } from 'next/navigation'
 import { useTranslation } from '@/hooks/useTranslation'
 import FormActionButtons from '@/components/FormActionButtons'
-import { nanoid } from 'nanoid'
 import MuiTextfieldWithoutControl from '@/components/inputs/MuiTextFieldWithoutControl'
 import moment from 'moment'
 import { useToast } from '@/hooks/useToast'
@@ -14,31 +13,14 @@ const RawMultiTransactions = ({ onClose = () => {} }) => {
 	const params = useParams()
 	const { t } = useTranslation(params?.lng ?? 'en', ['common', 'transactions'])
 	const { isLoading, createManyTransactions } = useTransaction()
-	const [allData, setAllData] = useState([{ id: nanoid(), description: '' }])
-	const [currentValue, setCurrentValue] = useState({ id: nanoid(), description: '' })
-	const delay = useDeferredValue(currentValue)
+	const [allData, setAllData] = useState('')
 	const { toastError } = useToast()
-
-	const updateLine = useCallback(() => {
-		const { description = '', id } = delay
-		setAllData((prev = []) => {
-			const index = prev.findIndex(i => i.id === id)
-			const result = [...prev]
-			if (index > -1) result[index] = { id, description }
-
-			return result
-		})
-	}, [delay])
-
-	useEffect(() => {
-		if (delay?.description && delay?.description !== '') updateLine(delay)
-	}, [delay, updateLine])
 
 	const onSubmit = useCallback(
 		async e => {
 			e?.preventDefault()
-			const result = allData.map((i, index) => {
-				const dataArray = i.description.split('|')
+			const result = allData.split('*').map((l, index) => {
+				const dataArray = l.split('|')
 
 				if (dataArray.length !== 5) return { error: `${t('transactions:missing_data_in_line')} ${index + 1}` }
 
@@ -66,7 +48,6 @@ const RawMultiTransactions = ({ onClose = () => {} }) => {
 				toastError(errorExists.error)
 				return
 			}
-
 			const response = await createManyTransactions(result)
 
 			if (response) onClose()
@@ -74,27 +55,20 @@ const RawMultiTransactions = ({ onClose = () => {} }) => {
 		[allData, createManyTransactions, onClose, t, toastError]
 	)
 
-	const addLine = useCallback(() => {
-		const id = nanoid()
-		setAllData(v => [...v, { id, description: '' }])
-	}, [])
-
 	return (
 		<SimpleModal onClose={onClose} title={t('common:create')} isLoading={isLoading}>
 			<Box sx={styles.container}>
 				<Box sx={styles.container}>
-					{allData.map((i, index) => (
-						<MuiTextfieldWithoutControl
-							key={i.id}
-							fieldName={'description'}
-							setState={description => setCurrentValue({ description, id: i.id })}
-							options={{ label: `${t('transactions:description')} ${index + 1}`, multiline: true }}
-						/>
-					))}
+					<MuiTextfieldWithoutControl
+						fieldName={'description'}
+						state={allData}
+						options={{
+							label: t('transactions:description'),
+							multiline: true,
+							onBlur: e => setAllData(e?.target?.value)
+						}}
+					/>
 				</Box>
-				<Button variant="outlined" onClick={addLine}>
-					{t('transactions:add')}
-				</Button>
 				<FormActionButtons onClose={onClose} onClick={onSubmit} />
 			</Box>
 		</SimpleModal>
