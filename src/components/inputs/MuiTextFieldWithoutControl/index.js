5252099d3d9f3063a8798d34dc7ff16296d7ee3f
David Rios <drios9107@gmail.com>
2026-03-16 14:04:35 -0400

    added onBlur behavior

--- src/components/inputs/MuiTextFieldWithoutControl/index.js
+++ src/components/inputs/MuiTextFieldWithoutControl/index.js
@@ -1,23 +1,29 @@
 import ShowFieldErrors from '@/components/ShowFieldErrors'
-import { FormControl, InputAdornment, TextField } from '@mui/material';
+import { FormControl, InputAdornment, TextField } from '@mui/material'
 
-const MuiTextfieldWithoutControl = ({ fieldName, state, setState = () => { }, errors, options = {} }) => {
-    return <FormControl>
-        <TextField
-            defaultValue={state}
-            onChange={v => setState(v?.target?.value)}
-            size='small'
-            rows={options?.multiline ? options?.rows ?? 3 : 1}
-            slotProps={{
-                input: {
-                    endAdornment: options?.endAdornment ? <InputAdornment position="end">{options?.endAdornment}</InputAdornment> : null,
-                    startAdornment: options?.startAdornment ? <InputAdornment position="start">{options?.startAdornment}</InputAdornment> : null
-                },
-            }}
-            {...options}
-        />
-        <ShowFieldErrors errors={errors} fieldName={fieldName} />
-    </FormControl>
+const MuiTextfieldWithoutControl = ({ fieldName, state, setState = () => {}, errors, options = {} }) => {
+	return (
+		<FormControl>
+			<TextField
+				defaultValue={state}
+				onChange={v => (!options.onBlur ? setState(v?.target?.value) : () => {})}
+				size="small"
+				rows={options?.multiline ? (options?.rows ?? 3) : 1}
+				slotProps={{
+					input: {
+						endAdornment: options?.endAdornment ? (
+							<InputAdornment position="end">{options?.endAdornment}</InputAdornment>
+						) : null,
+						startAdornment: options?.startAdornment ? (
+							<InputAdornment position="start">{options?.startAdornment}</InputAdornment>
+						) : null
+					}
+				}}
+				{...options}
+			/>
+			<ShowFieldErrors errors={errors} fieldName={fieldName} />
+		</FormControl>
+	)
 }
 
 export default MuiTextfieldWithoutControl
