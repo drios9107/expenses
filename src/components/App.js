f318825c58e358b39097297c5cbed6fb9a3b22a0
David Rios <drios9107@gmail.com>
2026-04-22 08:47:55 -0400

    added development badge

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,39 @@
-'use client';
-import { ListProvider } from '@/contexts/ListContext';
-import { LocalizationProvider } from '@mui/x-date-pickers';
-import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFnsV3';
-import { es } from 'date-fns/locale';
-import { DashboardProvider } from '@/contexts/DashboardContext';
-import NextTopLoader from 'nextjs-toploader';
-import Layout from './UserLayout/Layout';
-import { SessionProvider } from 'next-auth/react';
-import CookieConsentBanner from './CookieConsentBanner';
-import AuthGuard from './AuthGuard';
+'use client'
+import { ListProvider } from '@/contexts/ListContext'
+import { LocalizationProvider } from '@mui/x-date-pickers'
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFnsV3'
+import { es } from 'date-fns/locale'
+import { DashboardProvider } from '@/contexts/DashboardContext'
+import NextTopLoader from 'nextjs-toploader'
+import Layout from './UserLayout/Layout'
+import { SessionProvider } from 'next-auth/react'
+import CookieConsentBanner from './CookieConsentBanner'
+import AuthGuard from './AuthGuard'
+import InDevelopmentBadge from './InDevelopmentBadge'
+import { usePathname } from 'next/navigation'
+import { useMemo } from 'react'
 
 const App = ({ children }) => {
-    return <SessionProvider>
-        <AuthGuard>
-            <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={es}>
-                <DashboardProvider>
-                    <ListProvider>
-                        <NextTopLoader showSpinner={false} />
-                        <CookieConsentBanner />
-                        <Layout>
-                            {children}
-                        </Layout>
-                    </ListProvider>
-                </DashboardProvider>
-            </LocalizationProvider>
-        </AuthGuard>
-    </SessionProvider>
+	const pathname = usePathname()
+
+	const isOnlyTop = useMemo(() => ['/', '/en', '/es'].includes(pathname), [pathname])
+
+	return (
+		<SessionProvider>
+			<InDevelopmentBadge isOnlyTop={!isOnlyTop} />
+			<AuthGuard>
+				<LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={es}>
+					<DashboardProvider>
+						<ListProvider>
+							<NextTopLoader showSpinner={false} />
+							<CookieConsentBanner />
+							<Layout>{children}</Layout>
+						</ListProvider>
+					</DashboardProvider>
+				</LocalizationProvider>
+			</AuthGuard>
+		</SessionProvider>
+	)
 }
 
 export default App
--- src/components/InDevelopmentBadge.js
+++ src/components/InDevelopmentBadge.js
@@ -1,13 +1,21 @@
-import { Error } from '@mui/icons-material';
-import { Chip } from '@mui/material';
+import { Error } from '@mui/icons-material'
+import { Chip } from '@mui/material'
 
-const InDevelopmentBadge = () => {
-    return <Chip sx={styles.chip} label='In Development' color='error' variant='outlined' icon={<Error fontSize='small' />} />
+const InDevelopmentBadge = ({ isOnlyTop = false }) => {
+	return (
+		<Chip
+			sx={isOnlyTop ? styles.isOnlyTop : styles.chip}
+			label="In Development"
+			color="error"
+			variant="outlined"
+			icon={<Error fontSize="small" />}
+		/>
+	)
 }
 
 export default InDevelopmentBadge
 
-
 const styles = {
-    chip: { position: 'absolute', top: '10px', right: '10px', fontWeight: 'bold' }
+	chip: { position: 'absolute', top: '10px', right: '10px', fontWeight: 'bold' },
+	isOnlyTop: { position: 'absolute', top: '10px', left: '50%', transform: 'translateX(-50%)', fontWeight: 'bold' }
 }
