34dd0c2fa96b849b49833595dd1d9e90486b7205
David Rios <drios9107@gmail.com>
2026-05-15 10:26:59 -0400

    fixed DetailsDrawer styles

--- src/components/DetailsDrawer.js
+++ src/components/DetailsDrawer.js
@@ -1,20 +1,37 @@
 import { Box, Divider, Drawer, Typography } from '@mui/material'
 import React from 'react'
 
-const DetailsDrawer = ({ children, onClose = () => { }, extraclasses = {} }) => {
-    return <Drawer open onClose={onClose}>
-        <Box sx={styles.container}>
-            <Typography sx={{ alignSelf: 'center' }}>Details</Typography>
-            <Divider />
-            <Box sx={extraclasses}>
-                {children}
-            </Box>
-        </Box>
-    </Drawer>
+const DetailsDrawer = ({ children, onClose = () => {}, extraclasses = {} }) => {
+	return (
+		<Drawer
+			open
+			onClose={onClose}
+			slotProps={{
+				paper: {
+					sx: {
+						borderTopRightRadius: '16px',
+						borderBottomRightRadius: '16px'
+					}
+				}
+			}}
+		>
+			<Box sx={styles.container}>
+				<Typography sx={{ alignSelf: 'center' }}>Details</Typography>
+				<Divider />
+				<Box sx={extraclasses}>{children}</Box>
+			</Box>
+		</Drawer>
+	)
 }
 
 export default DetailsDrawer
 
 const styles = {
-    container: { display: 'flex', flexDirection: 'column', gap: '25px', p: '25px', width: '300px' },
+	container: {
+		display: 'flex',
+		flexDirection: 'column',
+		gap: '25px',
+		p: '25px',
+		width: '300px'
+	}
 }
