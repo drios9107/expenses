5063a183df1af619151f5da54fde1727dd43ca59
David Rios <drios9107@gmail.com>
2026-05-17 15:00:57 -0400

    added styles in loading component

--- src/app/[lng]/loading.js
+++ src/app/[lng]/loading.js
@@ -1,9 +1,17 @@
 'use client'
 
-import { CircularProgress } from '@mui/material'
+import { Box, CircularProgress } from '@mui/material'
 
 const LoadingPage = () => {
-	return <CircularProgress />
+	return (
+		<Box sx={styles.container}>
+			<CircularProgress />
+		</Box>
+	)
 }
 
 export default LoadingPage
+
+const styles = {
+	container: { width: '100%', height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }
+}
